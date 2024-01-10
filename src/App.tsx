c11f4168b72a1857893a02b0176df3bd2c5a2717
suhaasvijay <suhaasvijay7@gmail.com>
2024-01-10 19:18:38 +0530

    add title

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
     <>
       <div className='bg-white p-5 rounded-3xl flex flex-col items-center'>
         <canvas id='canvas' className='p-4 bg-black mb-4 rounded-3xl'></canvas>
-        <img src='/public/image-qr-code.png' alt='qr-code' className='rounded-3xl' width={300} height={300} />
+        <img src='/image-qr-code.png' alt='qr-code' className='rounded-3xl' width={300} height={300} />
         <div className='max-w-[300px] flex flex-wrap'>
           <h4 className='text-black text-2xl font-bold pt-[40px] '>
             Improve your front-end skills by building projects
