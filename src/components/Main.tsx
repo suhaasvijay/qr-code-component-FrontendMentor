544b0fe0f18c43aeaa62e75515830d4b8dbd4d29
suhaasvijay <suhaasvijay7@gmail.com>
2024-01-10 23:39:35 +0530

    added min height to textarea

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -40,8 +40,8 @@ export default function Main() {
         <h1 className='font-bold text-3xl px-5 md:text-6xl '>
           Generate your QR code in a few clicks
         </h1>
-        <div className='flex gap-5 mt-16 px-5 items-center wrapper'>
-          <textarea placeholder='Please enter text' value={input} onChange={handleInputChange} className='text-black !outline-none text-base w-[150%] font-semibold border-2 border-grey-300 p-2 rounded-md' />
+        <div className='flex gap-5 mt-16 px-5 items-start wrapper'>
+          <textarea placeholder='Please enter text' value={input} onChange={handleInputChange} className='text-black !outline-none text-base w-[150%] font-semibold border-2 border-grey-300 p-2 rounded-md min-h-[100px]' />
           {input && <button onClick={handleOnClickClear} className='bg-gray-500 button1 flex items-center justify-center text-white h-[50%] text-sm rounded-full'>X</button>}
 
           <button className='bg-green-500 text-white py-2 px-4 rounded-md' onClick={generateQR}>Generate</button>
