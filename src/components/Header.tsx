bb77a581f18b4a63ad3bb6b6db23d9cc035af8e3
suhaasvijay <suhaasvijay7@gmail.com>
2024-01-10 23:30:12 +0530

    made the webpage responsive and add clear button and toast

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,17 @@
+import toast, { Toaster } from 'react-hot-toast';
+
 export default function Header() {
+
+  const handleLogin = () => {
+    toast.error("Under Development")
+  }
   return (
-    <div className="max-w-6xl mx-auto flex justify-between items-center">
+    <div className="max-w-6xl mx-auto flex justify-between items-center px-5">
       <h1 className='text-xl font-bold text-left py-5'>QR Code Generator</h1>
-      <button className="text-md font-semibold bg-green-400 py-2 px-4 rounded-md text-white">Login</button>
+      <button onClick={handleLogin} className="text-md font-semibold bg-green-400 py-2 px-4 rounded-md text-white">Login</button>
+      <Toaster
+        position="bottom-right"
+        reverseOrder={false} />
     </div>
   )
 }
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import QRCode from 'qrcode'
 import { saveAs } from 'file-saver'
+import toast, { Toaster } from 'react-hot-toast';
 
 import '../App.css'
 
@@ -25,31 +26,41 @@ export default function Main() {
 
   const downloadImage = () => {
     saveAs(qrcode, 'image.jpg')
+    toast.success("Image Downloaded!")
+  }
+
+  const handleOnClickClear = () => {
+    setInput('')
+    setQrcode('')
   }
 
   return (
-    <div className='max-w-6xl mx-auto rounded-3xl flex justify-between items-center mt-32'>
-      <div className=' flex flex-col justify-normal items-left gap-2 max-w-[400px]'>
-        <h1 className='font-bold text-6xl'>
+    <div className='max-w-6xl mx-auto rounded-3xl flex flex-col md:flex-row justify-between items-center mt-32 gap-16 md:gap-0 md:px-5'>
+      <div className=' flex flex-col justify-normal items-left gap-2 max-w-[500px]'>
+        <h1 className='font-bold text-3xl px-5 md:text-6xl '>
           Generate your QR code in a few clicks
         </h1>
-        <div className='flex gap-5 mt-16'>
-          <input type="text" placeholder='Please enter text' value={input} onChange={handleInputChange} className='text-black text-base w-[150%] font-semibold border-2 border-grey-300 p-2 rounded-md' />
+        <div className='flex gap-5 mt-16 px-5 items-center wrapper'>
+          <input type="text" placeholder='Please enter text' value={input} onChange={handleInputChange} className='text-black !outline-none text-base w-[150%] font-semibold border-2 border-grey-300 p-2 rounded-md' />
+          <button onClick={handleOnClickClear} className='bg-gray-500 button1 flex items-center justify-center text-white h-[50%] text-sm rounded-full'>X</button>
           <button className='bg-green-500 text-white py-2 px-4 rounded-md' onClick={generateQR}>Generate</button>
         </div>
       </div>
-      <div className='image p-10 rounded-2xl flex flex-col gap-10'>
+      <div className='image p-10 rounded-2xl flex flex-col gap-10 mb-5 md:pb-0 mx-5 sm:mx-0'>
         {qrcode ?
           <img src={qrcode} alt="Qrcode" width='200px' height='auto' className='rounded-2xl' />
           :
-          <img src='/placeholder-qr.png' alt="Qrcode" width='200px' height='auto' className='rounded-2xl  blur-lg' />
+          <img src='/placeholder-qr.png' alt="Qrcode" width='200px' height='auto' className='rounded-2xl blur-lg' />
         }
         {qrcode ?
-          <button onClick={downloadImage} className='bg-green-500 text-white p-2 rounded-md'>Download</button>
+          <button onClick={downloadImage} className='bg-green-500 text-white p-2 rounded-md md:mb-10'>Download</button>
           :
-          <button disabled className='bg-green-900  text-white p-2 rounded-md'>Download</button>
+          <button disabled className='bg-green-900  text-white p-2 rounded-md md:mb-10'>Download</button>
         }
       </div>
+      <Toaster
+        position="bottom-right"
+        reverseOrder={false} />
     </div>
   )
 }
